6926fe7f18e3f69485072a65342112f57d5062c4
mattwecht <mattwecht@gmail.com>
2018-07-31 12:27:28 +0000

    automatically backed up by learn

---
+++ icecream.rb
@@ -0,0 +1,17 @@
+puts "what would you like to have the ice cream in?"
+base = gets.chomp
+puts "what is your first layer of ice cream?"
+firstlayer = gets.chomp 
+puts "what is your second layer of ice cream?"
+secondlayer = gets.chomp 
+puts "and a third layer?"
+thirdlayer = gets.chomp 
+puts "hot fudge or no hot fudge?"
+fudge = gets.chomp 
+puts "chocolate or rainbow sprinkles?"
+sprinkles = gets.chomp 
+puts "any one candy topping?"
+candy = gets.chomp
+puts "cherry or no cherry?"
+cherry = gets.chomp 
+puts "If i could have an ideal ice cream sundae right now, i would have it in a #{base}. I would then have #{firstlayer} on the bottom, #{secondlayer} in the middle, and #{thirdlayer} on top. For toppings I would have #{fudge}, #{sprinkles}, and #{candy}. Lastly, I would top the sundae off with #{cherry}. "
---
+++ introduction.rb
@@ -0,0 +1,7 @@
+def introduction(name)
+  puts "Hello, my name is #{name}"
+end 
+
+introduction()
+introduction("Matt the Rockstar Instructor")
+introduction("S$ and Ronke the Birthday Girls")
